c087eeabbc55996dd96e4ea842283cd8e3e91ea1
Becca Nelson <beccanelson88@gmail.com>
2016-05-18 15:04:03 -0500

    Add test runner

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,2 +1,8 @@
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin={"pretty"}, glue="com.rnelson.server", features="src/test/resources/cucumber")
 public class TestRunner {
 }
--- src/test/java/com/rnelson/server/Steps.java
+++ src/test/java/com/rnelson/server/Steps.java
@@ -1,8 +1,9 @@
+package com.rnelson.server;
+
+import com.rnelson.server.Server;
 import cucumber.api.PendingException;
-import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import cucumber.api.java.en.*;
+
 
 public class Steps {
 
