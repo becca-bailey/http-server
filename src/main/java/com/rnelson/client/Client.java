ec38e78828b89698b4da64c29ece64428f7902e4
Becca Nelson <beccanelson88@gmail.com>
2016-05-27 17:18:16 -0500

    Fix loops to work in browser

--- src/main/java/com/rnelson/client/Client.java
+++ src/main/java/com/rnelson/client/Client.java
@@ -3,7 +3,7 @@ package com.rnelson.client;
 public class Client {
 
     public static void main(String[] args) throws Exception {
-        ClientRunner runner = new ClientRunner("localhost", 5000);
+        ClientRunner runner = new ClientRunner("localhost", 8000);
         runner.run();
     }
 }
--- src/main/java/com/rnelson/server/ServerRunner.java
+++ src/main/java/com/rnelson/server/ServerRunner.java
@@ -16,26 +16,27 @@ public class ServerRunner implements Runnable {
 
     private void echo (PrintWriter out, BufferedReader in) throws IOException {
         String request;
-        while((request = in.readLine()) != null) {
-            out.println(echoResponse(request));
-        }
+        request = in.readLine();
+        out.println(echoResponse(request));
     }
 
     @Override
     public void run() {
         System.out.println("Listening on port " + serverPort + "...");
-        try (
-                ServerSocket serverSocket = new ServerSocket(serverPort);
-                Socket clientSocket = serverSocket.accept();
+        while (true) {
+            try (
+                    ServerSocket serverSocket = new ServerSocket(serverPort);
+                    Socket clientSocket = serverSocket.accept();
 
-                PrintWriter out =
-                        new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in =
-                        new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
-        ) {
-            this.echo(out, in);
-        } catch (IOException e) {
-            e.printStackTrace();
+                    PrintWriter out =
+                            new PrintWriter(clientSocket.getOutputStream(), true);
+                    BufferedReader in =
+                            new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+            ) {
+                this.echo(out, in);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
