2e564f1a66e9b02cda94226b5a9053c87a2a77ac
Becca Nelson <beccanelson88@gmail.com>
2016-06-02 14:39:55 -0500

    Add 404 response (test throws exception)

--- src/main/java/com/rnelson/server/RequestHandler.java
+++ src/main/java/com/rnelson/server/RequestHandler.java
@@ -34,11 +34,15 @@ public class RequestHandler {
     public String getResponse() {
         String response = "";
         String okayStatus = "HTTP/1.1 200 OK\r\n\r\n";
+        String notFound = "HTTP/1.1 404 Not Found\r\n\r\n";
         if (routes.contains(parameter())) {
             response += okayStatus;
         }
+        else {
+            response = notFound;
+        }
         if (parameter().equals("/echo")) {
-            response = okayStatus + request;
+            response += request;
         }
         return response;
     }
