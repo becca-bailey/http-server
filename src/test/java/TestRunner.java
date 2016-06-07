a919b6f25d361896de6f638a5393b28f0fa74569
Becca Nelson <beccanelson88@gmail.com>
2016-06-07 09:46:02 -0500

    edit test runner

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,9 +1,11 @@
+import com.rnelson.server.RequestHandler;
+import com.rnelson.server.unitTests.RequestHandlerTests;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin={"pretty"}, glue="com.rnelson.server", features="src/test/resources/cucumber")
+@CucumberOptions(plugin={"pretty"}, glue="com.rnelson.server", features="src/test/resources")
 
 public class TestRunner {
 }
