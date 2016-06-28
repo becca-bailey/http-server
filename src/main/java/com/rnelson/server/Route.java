335cb5a0427ca050b16c559bcc1b45db039e6a76
Becca Nelson <beccanelson88@gmail.com>
2016-06-27 21:47:51 -0500

    Router can return controller class

--- src/main/java/com/rnelson/server/Route.java
+++ src/main/java/com/rnelson/server/Route.java
@@ -1,14 +1,23 @@
 package com.rnelson.server;
 
+import com.rnelson.server.utilities.SharedUtilities;
+
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Route {
     String url;
+    String rootName = "Root";
     Set<String> methods = new HashSet<String>();
 
     public Route(String url) {
         this.url = url;
+    }
+
+    public Route(String url, String defaultRootName) {
+        this.url = url;
+        this.rootName = defaultRootName;
     }
 
     public void addMethod(String method) {
@@ -21,5 +30,18 @@ public class Route {
 
     public int countMethods() {
         return methods.size();
+    }
+
+    public List<String> getPaths() {
+        return SharedUtilities.findAllMatches("\\w+", url);
+    }
+
+    public String getClassName() {
+        List<String> paths = getPaths();
+        if (paths.size() > 0) {
+            return SharedUtilities.capitalize(paths.get(0));
+        } else {
+            return rootName;
+        }
     }
 }
--- src/main/java/com/rnelson/server/Router.java
+++ src/main/java/com/rnelson/server/Router.java
@@ -1,12 +1,20 @@
 package com.rnelson.server;
 
+import com.rnelson.server.utilities.exceptions.ControllerException;
 import com.rnelson.server.utilities.exceptions.RouterException;
+import org.apache.commons.io.FilenameUtils;
 
+import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Router {
     Set<Route> routes = new HashSet<Route>();
+    File rootDirectory;
+
+    public Router (File rootDirectory) {
+        this.rootDirectory = rootDirectory;
+    }
 
     public void addRoute(String method, String url) {
         Route route = null;
@@ -31,5 +39,41 @@ public class Router {
 
     public int countRoutes() {
         return routes.size();
+    }
+
+    public File[] listControllers() {
+        File controllersDirectory = new File(rootDirectory.getPath() + "/controllers");
+        return controllersDirectory.listFiles();
+    }
+
+    public Object getControllerForRoute(String url) throws ControllerException {
+        try {
+            Route route = getExistingRoute(url);
+            File[] controllerFiles = listControllers();
+            String expectedClassName = expectedControllerClass(route.getClassName());
+            for (File file : controllerFiles) {
+                String fileName = FilenameUtils.removeExtension(file.getName());
+                if (controllerMatchesFilename(fileName, expectedClassName)) {
+                    Class controllerClass = Class.forName(getPackageNameFromFileName(fileName));
+                    return controllerClass.newInstance();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        throw new ControllerException("Controller not found. Server is looking for '/controllers/<This>Controller.java' in the root directory.");
+    }
+
+    private String getPackageNameFromFileName(String fileName) {
+        return "com.rnelson.server.controllers." + FilenameUtils.removeExtension(fileName);
+        // fix this with a config file
+    }
+
+    private String expectedControllerClass(String className) {
+        return className + "Controller";
+    }
+
+    private Boolean controllerMatchesFilename (String fileName, String expectedClassName) {
+        return fileName.equals(expectedClassName);
     }
 }
