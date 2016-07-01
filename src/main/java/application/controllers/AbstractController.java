edaff96d3a87a5c059c0c530d24ebc779dfd1377
Becca Nelson <beccanelson88@gmail.com>
2016-07-01 16:17:13 -0500

    Move unused methods from CoffeeController to AbstractController

--- src/main/java/application/controllers/AbstractController.java
+++ src/main/java/application/controllers/AbstractController.java
@@ -4,7 +4,32 @@ import com.rnelson.server.Controller;
 import com.rnelson.server.utilities.Response;
 
 public abstract class AbstractController implements Controller {
+
+    public byte[] get() {
+        return Response.methodNotAllowed.getBytes();
+    }
+
     public byte[] head() {
+        return Response.methodNotAllowed.getBytes();
+    }
+
+    public byte[] post() {
+        return Response.methodNotAllowed.getBytes();
+    }
+
+    public byte[] put() {
+        return Response.methodNotAllowed.getBytes();
+    }
+
+    public byte[] patch() {
+        return Response.methodNotAllowed.getBytes();
+    }
+
+    public byte[] options() {
+        return Response.methodNotAllowed.getBytes();
+    }
+
+    public byte[] delete() {
         return Response.methodNotAllowed.getBytes();
     }
 }
--- src/main/java/application/controllers/CoffeeController.java
+++ src/main/java/application/controllers/CoffeeController.java
@@ -2,7 +2,6 @@ package application.controllers;
 
 import application.Header;
 import com.rnelson.server.ResponseData;
-import com.rnelson.server.utilities.Response;
 import com.rnelson.server.utilities.SharedUtilities;
 
 public class CoffeeController extends AbstractController {
@@ -13,31 +12,6 @@ public class CoffeeController extends AbstractController {
         Header header = new Header(418);
         byte[] responseHeader = header.getResponseHeader();
         return SharedUtilities.addByteArrays(responseHeader, body);
-    }
-
-    @Override
-    public byte[] post() {
-        return Response.methodNotAllowed.getBytes();
-    }
-
-    @Override
-    public byte[] put() {
-        return Response.methodNotAllowed.getBytes();
-    }
-
-    @Override
-    public byte[] patch() {
-        return Response.methodNotAllowed.getBytes();
-    }
-
-    @Override
-    public byte[] options() {
-        return Response.methodNotAllowed.getBytes();
-    }
-
-    @Override
-    public byte[] delete() {
-        return Response.methodNotAllowed.getBytes();
     }
 
     @Override
